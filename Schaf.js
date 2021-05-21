0e4319bba2f57d67b8cc9c84181c8dde151dd137
das-rossbacher-schaf <84539106+das-rossbacher-schaf@users.noreply.github.com>
2021-05-21 13:57:02 +0200

    Add files via upload

--- Schaf.js
+++ Schaf.js
@@ -5,3 +5,16 @@ function video_appears() {
           document.getElementById("Schaf").style.opacity = (10-i)/10;
      }
 }
+
+function play_audio() {
+     document.getElementById("maehen").play();
+}
+
+var video = document.getElementById("background_video");
+video.onended = function () {
+     alert("hallo");
+     /*
+     document.getElementById("background_video").style.visibility = "hidden";
+     document.getElementById("Schaf").style.opacity = 1.0;
+     */
+};
